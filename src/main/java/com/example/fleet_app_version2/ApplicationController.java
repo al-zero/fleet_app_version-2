166907fb7e73adc230a042040f709923129fc145
alpha sabawu <46676904+al-zero@users.noreply.github.com>
2022-11-19 18:19:17 +0200

    dashboard  working

--- src/main/java/com/example/fleet_app_version2/ApplicationController.java
+++ src/main/java/com/example/fleet_app_version2/ApplicationController.java
@@ -10,4 +10,9 @@ public class ApplicationController {
     public String home(){
         return "index";
     }
+
+    @GetMapping("/index2")
+    public String widget(){
+        return "index2";
+    }
 }
