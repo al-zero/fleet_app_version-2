b27d602b73041415431e1c5dffe4985bc0480de1
alpha sabawu <46676904+al-zero@users.noreply.github.com>
2022-11-28 13:53:35 +0200

    added country

--- src/main/java/com/example/fleet_app_version2/parameters/controllers/CountryController.java
+++ src/main/java/com/example/fleet_app_version2/parameters/controllers/CountryController.java
@@ -1,12 +1,12 @@
 package com.example.fleet_app_version2.parameters.controllers;
 
-
 import com.example.fleet_app_version2.parameters.models.Country;
 import com.example.fleet_app_version2.parameters.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
 @Controller
@@ -20,5 +20,16 @@ public class CountryController {
         List<Country> countryList = countryService.getAllCountries();
         model.addAttribute("countries", countryList );
         return "parameters/countries";
+    }
+
+    @GetMapping("/countryAdd")
+    public String addCountry(){
+        return "parameters/countryAdd";
+    }
+
+    @PostMapping("/countries")
+    public  String saveCountry(Country country){
+            countryService.save(country);
+            return  "redirect:/countries";
     }
 }
